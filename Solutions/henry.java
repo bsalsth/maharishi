b6f0bebfb45c494ffc4ed0947df3754bb309f6d6
bsalsth <bsalbizi@gmail.com>
2015-12-01 19:57:10 +0545

    This is correct one for henry.java .
    Previously it was limited for few arguments.

--- Solutions/henry.java
+++ Solutions/henry.java
@@ -1,30 +1,33 @@
-public class henry{
-	public static void main(String[] args){
-		int result = henry(1, 3);
-		System.out.println(result);
-	}
 
-	static int henry(int i, int j){
-		int henry = 0;
-		int max = Integer.MAX_VALUE;
-		int perfectCount = 0;
-		for(int number=1; number <= max; number++){
-			int sum = 0;
-			for(int divider = 1; divider < number; divider++){
-				if(number % divider == 0){
-					sum += divider;
-				}
-			}
-			if(sum == number){
-				perfectCount++;
-				if(perfectCount == i){
-					henry += sum;
-				}else if(perfectCount == j){
-					henry += sum;
-					break;
-				}
-			}
+	static int henry (int i, int j){
+		int max =0, min = 0 , first = 0, second = 0, counter = 0;
+		if(i > j){
+			max = i;
+			min = j;
 		}
-		return henry;
+		else {
+			max = j;
+			min = i;
+		}
+		int k = 5;
+		while(counter != max){
+			if(isPerfectNo(k)){
+				counter++;
+				if(counter == min)
+					first = k;
+			}
+			k++;
+		}
+		second = k-1;
+		return first+second;
+
 	}
-}
+	static boolean isPerfectNo(int n){
+		int sum = 0;
+		for(int i = 1; i <= n/2; i++){
+			if(n % i == 0)
+				sum += i;
+		}
+		if(sum == n) return true;
+		return false;
+	}
